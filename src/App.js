7e3f74d856b1973d034f824033046ea5f117fe4e
dh_bug <dh_bug@naver.com>
2023-10-31 12:15:23 +0900

    jsx 연습

--- src/App.js
+++ src/App.js
@@ -2,9 +2,32 @@
 function App() {
   return (
     <div >
+      <MyComponent1 />
 
+      <MyComponent2 />
     </div>
   );
+}
+
+function MyComponent1(){
+  return (
+    <>
+        <h1>Hello Component</h1>
+    </>
+  )
+}
+function MyComponent2(){
+    return (
+        <>
+             {/*p>lorem5^ul>li*3>lorem1*/}
+            <p>Lorem ipsum dolor sit amet.</p>
+            <ul>
+                <li>Lorem.</li>
+                <li>Delectus.</li>
+                <li>Eius?</li>
+            </ul>
+        </>
+    )
 }
 
 export default App;

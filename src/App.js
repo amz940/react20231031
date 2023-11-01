b396982ece86deb6cef94f536c9d73cea8c0773b
dh_bug <dh_bug@naver.com>
2023-11-01 11:22:11 +0900

    props 기본값 설정

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 function App() {
   return (
     <>
-      <MyComp value={"흥민"} address={"신촌"} />
-      <MyComp value={"강인"} address={"강남"} />
+      <MyComp name={"흥민"} age={33} address={"서울"} city={"부산"} />
+      <MyComp address={"강남"} city={"서울"} />
+      <MyComp address={"프랑스"} name="강인" age={33} city={"파리"} />
     </>
   );
 }
 
-function MyComp({ value, address }) {
+function MyComp({ name = "anonymous", address, city, age = 33 }) {
   return (
     <div>
-      <h1>
-        {value}는 {address}에 산다
-      </h1>
+      <p>
+        {name}은 {age}세 이고 {address},{city}에 산다
+      </p>
     </div>
   );
 }

b6fbbc4cabba58e819209f0a9dcb94a5f32f98ba
dh_bug <dh_bug@naver.com>
2023-11-02 15:36:44 +0900

    image 사용 방법

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import {
-  Box,
-  Center,
-  Flex,
-  FormControl,
-  FormLabel,
-  Switch,
-  Textarea,
-} from "@chakra-ui/react";
+import { Box, Image } from "@chakra-ui/react";
 
 function App(props) {
   return (
     <div>
       <Box w={"480px"}>
-        <FormControl display={"flex"} alignItem={"center"}>
-          <FormLabel mb={0}>알림을 받으시겠습니까?</FormLabel>
-          <Switch size={"md"} colorScheme="orange" />
-        </FormControl>
+        <image src="https:bit.ly/dan-abramov" />
       </Box>
+      <Box>
+        <Image boxSize={"100px"} src="https:bit.ly/dan-abramov" />
+      </Box>
+      <Box>
+        <Image boxSize={"200px"} src="https:bit.ly/dan-abramov" />
+      </Box>
+      <Box>
+        <Image
+          borderRadius="full"
+          boxSize="200px"
+          src="https:bit.ly/dan-abramov"
+        />
+      </Box>
+      <Box>
+        <Image src="abce.jpg" fallbackSrc="https://via.placeholder.com/150" />
+      </Box>
+      <Box></Box>
+      <Box></Box>
+      <Box></Box>
+      <Box></Box>
+      <Box></Box>
+      <Box></Box>
     </div>
   );
 }

2d02b2369b0ad25a5bb09a016501bc2cfd998c2b
dh_bug <dh_bug@naver.com>
2023-11-02 09:34:14 +0900

    필요없는 파일 삭제

--- src/App.js
+++ src/App.js
@@ -1,19 +1,5 @@
-// style 적용 방법
-// 보통의 css 파일
-// css module 사용
-
-import { MyComp } from "./componant/MyComp";
-import { MyBox } from "./componant/MyBox";
-import { MyContainer } from "./componant/MyContainer";
-
 function App() {
-  return (
-    <>
-      <MyComp />
-      <MyBox />
-      <MyContainer />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
--- src/componant/MyComp.js
+++
@@ -1,7 +0,0 @@
-export function MyComp() {
-  return (
-    <div>
-      <h1 style={{ color: "gold", backgroundColor: "blue" }}>lorem ipsum</h1>
-    </div>
-  );
-}

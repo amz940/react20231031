5e653fbf4fa7c9283feb3151931e49ff6e05433b
dh_bug <dh_bug@naver.com>
2023-11-02 15:31:08 +0900

    switch 만드는 법

--- src/App.js
+++ src/App.js
@@ -1,56 +1,22 @@
 import {
   Box,
   Center,
-  Checkbox,
-  CheckboxGroup,
   Flex,
   FormControl,
-  FormHelperText,
   FormLabel,
-  Input,
-  Radio,
-  RadioGroup,
+  Switch,
+  Textarea,
 } from "@chakra-ui/react";
 
-function App() {
+function App(props) {
   return (
     <div>
-      <Center>
-        <Box maxW={"480px"}>
-          <FormControl>
-            <FormLabel>이름</FormLabel>
-            <Input />
-            <FormHelperText>입력시 도움이 될만한 텍스트 입력</FormHelperText>
-          </FormControl>
-        </Box>
-      </Center>
-      <Center mt={5}>
-        <Box w={"480px"}>
-          <FormControl>
-            <FormLabel>하나만 골라라 좀</FormLabel>
-            <RadioGroup>
-              <Flex gap={5}>
-                <Radio value="value1">Lorem.</Radio>
-                <Radio value="value2">Lorem ipsum.</Radio>
-                <Radio value="value3">Lorem ipsum dolor.</Radio>
-                <Radio value="value4">Lorem ipsum dolor sit.</Radio>
-              </Flex>
-            </RadioGroup>
-          </FormControl>
-        </Box>
-      </Center>
-      <Center>
-        <Box w={"480px"}>
-          <FormControl>
-            <FormLabel>골라 잡숴봐</FormLabel>
-            <CheckboxGroup>
-              <Checkbox>Lorem.</Checkbox>
-              <Checkbox>Lorem ipsum.</Checkbox>
-              <Checkbox>Lorem.</Checkbox>
-            </CheckboxGroup>
-          </FormControl>
-        </Box>
-      </Center>
+      <Box w={"480px"}>
+        <FormControl display={"flex"} alignItem={"center"}>
+          <FormLabel mb={0}>알림을 받으시겠습니까?</FormLabel>
+          <Switch size={"md"} colorScheme="orange" />
+        </FormControl>
+      </Box>
     </div>
   );
 }

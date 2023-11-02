7b926885f561f14ab2c703b6b4ec8a042ac34577
dh_bug <dh_bug@naver.com>
2023-11-02 15:11:55 +0900

    form으로 묶어서 표시

--- src/App.js
+++ src/App.js
@@ -1,35 +1,57 @@
-import { Button } from "@chakra-ui/react";
-import { ArrowRightIcon, EmailIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Center,
+  Checkbox,
+  CheckboxGroup,
+  Flex,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Input,
+  Radio,
+  RadioGroup,
+} from "@chakra-ui/react";
 
 function App() {
   return (
-    <>
-      <Button>Lorem.</Button>
-      <Button colorScheme="green">Lorem.</Button>
-      <Button colorScheme="red">Dolore?</Button>
-      <Button colorScheme="orange">Vero?</Button>
-      <Button colorScheme="teal" size={"sm"}>
-        Aliquid.
-      </Button>
-      <Button colorScheme="facebook" size={"lg"}>
-        Veritatis?
-      </Button>
-      <Button colorScheme="whatsapp" variant={"outline"}>
-        Nulla?
-      </Button>
-      <Button colorScheme="messenger" variant={"ghost"}>
-        Nisi.
-      </Button>
-      <Button colorScheme="blackAlpha" variant={"link"}>
-        Repellat!
-      </Button>
-      <Button leftIcon={<EmailIcon />}>Quis?</Button>
-      <Button rightIcon={<ArrowRightIcon />}>Tenetur.</Button>
-      <Button isLoading>Velit!</Button>
-      <Button isLoading loadingText="지금, 곤란, 대기">
-        Unde.
-      </Button>
-    </>
+    <div>
+      <Center>
+        <Box maxW={"480px"}>
+          <FormControl>
+            <FormLabel>이름</FormLabel>
+            <Input />
+            <FormHelperText>입력시 도움이 될만한 텍스트 입력</FormHelperText>
+          </FormControl>
+        </Box>
+      </Center>
+      <Center mt={5}>
+        <Box w={"480px"}>
+          <FormControl>
+            <FormLabel>하나만 골라라 좀</FormLabel>
+            <RadioGroup>
+              <Flex gap={5}>
+                <Radio value="value1">Lorem.</Radio>
+                <Radio value="value2">Lorem ipsum.</Radio>
+                <Radio value="value3">Lorem ipsum dolor.</Radio>
+                <Radio value="value4">Lorem ipsum dolor sit.</Radio>
+              </Flex>
+            </RadioGroup>
+          </FormControl>
+        </Box>
+      </Center>
+      <Center>
+        <Box w={"480px"}>
+          <FormControl>
+            <FormLabel>골라 잡숴봐</FormLabel>
+            <CheckboxGroup>
+              <Checkbox>Lorem.</Checkbox>
+              <Checkbox>Lorem ipsum.</Checkbox>
+              <Checkbox>Lorem.</Checkbox>
+            </CheckboxGroup>
+          </FormControl>
+        </Box>
+      </Center>
+    </div>
   );
 }
 

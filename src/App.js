5dc0a8e24395c09a137b09e0cfeb95b294f6f4b3
dh_bug <dh_bug@naver.com>
2023-11-02 11:47:28 +0900

    card를 활용한 텍스트창 만들기

--- src/App.js
+++ src/App.js
@@ -1,48 +1,56 @@
-import { Box, Flex, SimpleGrid } from "@chakra-ui/react";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Flex,
+  Heading,
+  SimpleGrid,
+} from "@chakra-ui/react";
 
 function App() {
-  /*
-  base: "0em", // 0px
-  sm: "30em", // 1~480px. em is a relative unit and is dependant on the font size.
-  md: "48em", // 481~768px
-  lg: "62em", // 769~992px
-  xl: "80em", // 993~1280px
-  "2xl": "96em", // 1281~1536px
-   */
   return (
     <>
-      <SimpleGrid spacing={"10px"} columns={{ sm: 2, md: 3, "2xl": 6 }}>
-        {/*Box[bg="blue.300"]*30>lorem1*/}
-        <Box bg="blue.300">Lorem.</Box>
-        <Box bg="blue.300">Quis!</Box>
-        <Box bg="blue.300">Odit!</Box>
-        <Box bg="blue.300">Modi.</Box>
-        <Box bg="blue.300">Optio.</Box>
-        <Box bg="blue.300">Aspernatur!</Box>
-        <Box bg="blue.300">Id?</Box>
-        <Box bg="blue.300">Id.</Box>
-        <Box bg="blue.300">Quidem?</Box>
-        <Box bg="blue.300">Nobis!</Box>
-        <Box bg="blue.300">Atque.</Box>
-        <Box bg="blue.300">Modi!</Box>
-        <Box bg="blue.300">Consectetur?</Box>
-        <Box bg="blue.300">Quibusdam.</Box>
-        <Box bg="blue.300">Perspiciatis?</Box>
-        <Box bg="blue.300">Consequatur?</Box>
-        <Box bg="blue.300">Odit?</Box>
-        <Box bg="blue.300">Pariatur.</Box>
-        <Box bg="blue.300">Placeat!</Box>
-        <Box bg="blue.300">Odit?</Box>
-        <Box bg="blue.300">Inventore.</Box>
-        <Box bg="blue.300">Ducimus.</Box>
-        <Box bg="blue.300">Reiciendis?</Box>
-        <Box bg="blue.300">Ipsam!</Box>
-        <Box bg="blue.300">Labore?</Box>
-        <Box bg="blue.300">Ea?</Box>
-        <Box bg="blue.300">Ad.</Box>
-        <Box bg="blue.300">Vero.</Box>
-        <Box bg="blue.300">Mollitia?</Box>
-        <Box bg="blue.300">Velit!</Box>
+      <SimpleGrid gap={3} columns={{ base: 1, md: 3, xl: 6, "2xl": 8 }}>
+        {/*Card*3>CardHeader>Heading[size="md"]>lorem2^^CardBody>lorem15^CardFooter>Button>lorem1*/}
+        <Card>
+          <CardHeader>
+            <Heading size="md">Lorem ipsum.</Heading>
+          </CardHeader>
+          <CardBody>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Consequatur doloremque possimus repellendus suscipit voluptatibus.
+            Odio!
+          </CardBody>
+          <CardFooter>
+            <Button>Lorem.</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md">Aperiam, assumenda.</Heading>
+          </CardHeader>
+          <CardBody>
+            Alias aperiam architecto beatae ducimus, nostrum quas, quidem quis
+            recusandae, repellat sapiente sunt vel voluptates!
+          </CardBody>
+          <CardFooter>
+            <Button>Temporibus!</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md">Ex, harum?</Heading>
+          </CardHeader>
+          <CardBody>
+            Adipisci aperiam est harum magnam maxime omnis quidem similique vel
+            voluptatem voluptatibus. Accusamus, mollitia, voluptatem?
+          </CardBody>
+          <CardFooter>
+            <Button>Ex.</Button>
+          </CardFooter>
+        </Card>
       </SimpleGrid>
     </>
   );

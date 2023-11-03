06d9f5cf2b9b7f35f4816414888f74555bd235e9
dh_bug <dh_bug@naver.com>
2023-11-03 11:18:08 +0900

    prop으로 함수 전달하는 다양한 방법

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import { Button } from "@chakra-ui/react";
 
 function App(props) {
-  // 함수명 작성 관습
-  // handle 이벤트명을 붙인다
-  function handleClick() {
-    console.log("second");
+  function handleClick1() {
+    console.log("버튼 클릭됨");
+  }
+  function handleClick2() {
+    console.log("2번 버튼 클릭됨");
   }
 
-  function handleMouseEnter() {
-    console.log("third");
+  function handleButtonClick(number) {
+    console.log(number + "번째 버튼 클릭됨");
   }
 
-  function handleMouseLeave() {
-    console.log("4th");
-  }
+  let func3 = () => {
+    handleButtonClick(3);
+  };
+  let func4 = () => {
+    handleButtonClick(4);
+  };
 
   return (
     <div>
-      <Button onClick={() => console.log("first")}>버튼1</Button>
-      <Button onClick={handleClick}>버튼2</Button>
-      <Button onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-        버튼3
+      <Button onClick={handleClick1}>첫번째버튼</Button>
+      <Button onClick={handleClick2}>두번째버튼</Button>
+      <Button
+        onClick={() => {
+          handleButtonClick(3);
+        }}
+      >
+        세번째버튼
+      </Button>
+      <Button
+        onClick={() => {
+          handleButtonClick(4);
+        }}
+      >
+        네번째버튼
       </Button>
     </div>
   );

22a75ccb5ed8b51699601ec3d48a888a84477d66
dh_bug <dh_bug@naver.com>
2023-11-03 09:18:47 +0900

    children prop

--- src/App.js
+++ src/App.js
@@ -1,59 +1,20 @@
-import {
-  Box,
-  Button,
-  Flex,
-  Input,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuList,
-  Text,
-} from "@chakra-ui/react";
-import { ChevronDownIcon } from "@chakra-ui/icons";
-
 function App() {
   return (
-    <Flex
-      px={{ base: "0px", md: "70px", lg: "120px", xl: "150px" }}
-      justify="space-between"
-      py="8px"
-      my={"8px"}
-      bg={"blue.50"}
-    >
-      <Flex gap="7px" align={"center"}>
-        <Text as="b">Navbar</Text>
-        <Button variant={"ghost"}>Home</Button>
-        <Button variant={"ghost"}>Link</Button>
-
-        <Menu>
-          <MenuButton
-            variant="ghost"
-            as={Button}
-            rightIcon={<ChevronDownIcon />}
-          >
-            Dropdown
-          </MenuButton>
-          <MenuList>
-            <MenuItem>Download</MenuItem>
-            <MenuItem>Create a Copy</MenuItem>
-            <MenuItem>Mark as Draft</MenuItem>
-            <MenuItem>Delete</MenuItem>
-            <MenuItem>Attend a Workshop</MenuItem>
-          </MenuList>
-        </Menu>
-
-        <Button variant={"ghost"} isDisabled={true}>
-          Disabled
-        </Button>
-      </Flex>
-      <Flex gap="7px" align={"center"}>
-        <Input size="sm" bg="white" />
-        <Button size="sm" colorScheme="pink" variant="outline">
-          Search
-        </Button>
-      </Flex>
-    </Flex>
+    <>
+      <MyComp text="hello" address="seoul">
+        some contents 다른 컴포넌트가 존재 할 경우
+      </MyComp>
+    </>
   );
 }
 
+function MyComp({ text, address, children }) {
+  return (
+    <>
+      <p>{text}</p>
+      <p>{address}</p>
+      <p>{children}</p>
+    </>
+  );
+}
 export default App;

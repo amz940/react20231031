12fe522c29d61322e570655f27b77e8944dc1d76
dh_bug <dh_bug@naver.com>
2023-11-03 11:45:14 +0900

    event handler method에 event 객체가 전달된다

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import { Button } from "@chakra-ui/react";
+import { Input } from "@chakra-ui/react";
 
 function App(props) {
-  // 브라우저가 이벤트 핸들러 메소드에
-  // event 객체를 매개값으로 넣어준다 보통 줄여서 e로 넣는다
-  function handleClick(event) {
-    // console.log(event);
-    console.log(event.target);
-    // console.log(event.target.className);
-    // console.log(event.target.type);
+  function handleChange(e) {
+    // console.log("인풋값이 변경됨");
+    console.log(e.target.value);
   }
 
+  // onChange = 인풋에 입력되는 값 표현
   return (
     <div>
-      <Button colorScheme="orange" onClick={handleClick}>
-        button1
-      </Button>
-      <Button colorScheme="blue" onClick={handleClick}>
-        button2
-      </Button>
+      <Input onChange={handleChange} />
+      <Input onChange={handleChange} />
     </div>
   );
 }

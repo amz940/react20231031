54bcd85ce6359639bad20b910e3d8e399b21e04f
dh_bug <dh_bug@naver.com>
2023-11-03 15:18:20 +0900

    text input으로 상태변경

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import { Button, Text } from "@chakra-ui/react";
+import { Input, Text } from "@chakra-ui/react";
 import { useState } from "react";
 
 function App(props) {
-  // 0번째 인덱스는 상태
-  // 1번째 인덱스는 바뀌는 상태
-  let [number, setNumber] = useState(0);
-  function handleClick() {
-    // number++;
-    setNumber(number + 1);
-    // 2번 누를때 마다 하나씩 올라간다
-    // setNumber(number++);
-    console.log(number);
+  const [message, setMessage] = useState("");
+
+  function handleInputChange(e) {
+    setMessage(e.target.value);
   }
+
   return (
     <div>
-      <Button onClick={handleClick}>바꾸기</Button>
-      <Text>{number}</Text>
+      <Input onChange={handleInputChange} />
+      <Text>{message}</Text>
     </div>
   );
 }

e7ada924d20391dff74fe0770f7fe8be70b2b6b8
dh_bug <dh_bug@naver.com>
2023-11-06 12:33:06 +0900

    배열,객체를 상태로 쓸 경우 상태 변경 시 새로운 배열, 객체 로 복사해서 사용해야 한다

--- src/App.js
+++ src/App.js
@@ -1,46 +1,32 @@
-import React from "react";
-import { Box, List, ListItem } from "@chakra-ui/react";
+import { useState } from "react";
+import { Button, Input, List, ListItem } from "@chakra-ui/react";
 
 function App(props) {
-  const arr = ["pizza", "돈까스", "햄버거", "김치찌개"];
-  const arr2 = ["손흥민", "이강인", "김민재"];
-  const arr3 = ["라때", "아아", "디카페인"];
-  const arr4 = [
-    { id: 0, name: "라떼" },
-    { id: 1, name: "에스프레소" },
-    { id: 2, name: "카푸치노" },
-  ];
+  const [coffees, setCoffees] = useState(["라때", "아아"]);
+  const [text, setText] = useState("");
 
-  const Items = arr.map((item, index) => (
-    <ListItem key={index}>{item}</ListItem>
-  ));
-  // let map = arr2.map((abc) => <ListItem>{abc}</ListItem>);
+  function handleButtonClick() {
+    // 직전 상태 배열을 조작하면 변경이 안되고
+    // coffees.push(text);
+
+    // 새 배열을 만들어서 set state 해줘야 한다
+    const newCoffees = [...coffees]; // 배열 복사
+
+    newCoffees.push(text);
+    setCoffees(newCoffees);
+    console.log(coffees);
+  }
 
   return (
     <div>
-      <Box mb={3}>
-        {/*<List>*/}
-        {/*  <ListItem{arr[0]}></ListItem>*/}
-        {/*  <ListItem{arr[1]}></ListItem>*/}
-        {/*  <ListItem{arr[2]}></ListItem>*/}
-        {/*  <ListItem{arr[3]}></ListItem>*/}
-        {/*</List>*/}
-        <List>{Items}</List>
-      </Box>
-      <Box mb={3}>
-        <List>
-          {arr2.map((abc, index) => (
-            <ListItem key={index}>{abc}</ListItem>
-          ))}
-        </List>
-      </Box>
-      <Box mb={3}>
-        <List>
-          {arr3.map((e, i) => (
-            <ListItem key={i}>{e}</ListItem>
-          ))}
-        </List>
-      </Box>
+      <Input value={text} onChange={(e) => setText(e.target.value)} />
+      <Button onClick={handleButtonClick}>추가</Button>
+
+      <List>
+        {coffees.map((e, index) => (
+          <ListItem key={index}>{e}</ListItem>
+        ))}
+      </List>
     </div>
   );
 }

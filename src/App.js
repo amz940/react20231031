2f22f238397e236ea9d68a177792c0b3517310b4
dh_bug <dh_bug@naver.com>
2023-11-06 10:18:29 +0900

    버튼 클릭 횟수에 따른 메세지 테두리 변경

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
+import { Box, Button, Text } from "@chakra-ui/react";
 import { useState } from "react";
-import { Button } from "@chakra-ui/react";
 
 function App(props) {
-  const [text, setText] = useState("hello");
+  const [number, setNumber] = useState(0);
 
-  function handleButtonClick() {
-    // text라는 state를 값 변경
-    setText("greeting");
-  }
+  console.log(number);
+
+  const index = number % 3;
 
   return (
     <div>
-      <Button onClick={handleButtonClick}>상태 변경!</Button>
-      <p>{text}</p>
+      <Button onClick={() => setNumber(number + 1)}>다음</Button>
+      <Box borderWidth={"3px"} borderColor={index == 0 ? "blue" : "black"}>
+        <Text>message1</Text>
+      </Box>
+      <Box borderWidth={"3px"} borderColor={index == 1 ? "red" : "black"}>
+        <Text>message2</Text>
+      </Box>
+      <Box borderWidth={"3px"} borderColor={index == 2 ? "blue" : "black"}>
+        <Text>message3</Text>
+      </Box>
     </div>
   );
 }

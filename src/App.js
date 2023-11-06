c6d9325e137bfbeb6d0ffea63fbb972a8dc11533
dh_bug <dh_bug@naver.com>
2023-11-06 10:29:12 +0900

    hook 사용 조건 오류

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
+import { useState } from "react";
+
 function App(props) {
-  // HOOK : use로 시작하는 함수
-  // hook은 컴포넌트 상단에 작성해야만 한다
-  // 조건문이나, 반복문, 중첩된 함수안( function 안에 작성) 에 작성하면 안된다
+  const [number, setNumber] = useState(0);
 
-  if (true) {
-    // hook 호출 코드 작성 불가
-  }
-
-  for (let i = 0; i < 3; i++) {
-    // hook 호출 코드 작성 불가
-  }
-
-  function handle() {
-    // hook 호출 코드 작성 불가
+  if (number > 5) {
+    const [text, setText] = useState("");
   }
 
   return <div></div>;

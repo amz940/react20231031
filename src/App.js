52de91807024e2bb3046ff04be4ff87890a0793a
dh_bug <dh_bug@naver.com>
2023-11-08 10:13:21 +0900

    useEffect 예제

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import React, { useEffect, useState } from "react";
-import { Button, Input, Text } from "@chakra-ui/react";
+import { Box, Select, Text } from "@chakra-ui/react";
+import axios from "axios";
 
 function App(props) {
-  const [number, setNumber] = useState(0);
-  const [text, setText] = useState("");
+  const [customerId, setCustomerId] = useState(0);
+  const [customer, setCustomer] = useState({});
 
-  // 첫번째 파라미터(set up) : The function with your Effect's logic.
-  // 부작용이 있는 함수(부작용이 나쁜뜻의 부작용이 아니라 부수적인 작용을 한다는 뜻)
-  // 두번재 파라미터를 안 쓰면 재 렌더링 될때마다 사용된다
-  // 두번째 파라미터 : set up(첫번째 파라미터)을 실행 시키는 값의 나열(배열)
-  // 빈 배열이면 첫 렌더링 때만 실행된다, 빈 배열이 아니라면 첫 렌더링 + 배열의 변화가 있을ㄸ마다 사용된다
   useEffect(() => {
-    console.log("effect 함수 실행");
-  }, [number, text]);
+    axios
+      .get("/api/main1/sub4?id=" + customerId)
+      .then((response) => response.data)
+      .then((data) => setCustomer(data))
+      .catch((error) => console.log(error))
+      .finally(() => console.log("끝!"));
+  }, [customerId]);
 
   return (
     <div>
-      <Button onClick={() => setNumber(number + 1)}>증가</Button>
-      <Text>{number}</Text>
-      <Input value={text} onChange={(e) => setText(e.target.value)} />
+      <Select
+        placehorder="고객 번호를 선택하세요"
+        onChange={(e) => setCustomerId(e.target.value)}
+      >
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
+      </Select>
+      <Box>
+        <Text>고객 이름 : {customer.name}</Text>
+      </Box>
     </div>
   );
 }

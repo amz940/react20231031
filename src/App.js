3eb8e7a5f83a003e6651cbcff1a1f3a40a383b19
dh_bug <dh_bug@naver.com>
2023-11-08 14:31:16 +0900

    component의 초기 렌더링과 재 렌더링

--- src/App.js
+++ src/App.js
@@ -1,17 +1,59 @@
 import React, { useEffect, useState } from "react";
+import { Box, Button, Checkbox, Text } from "@chakra-ui/react";
+
+function MyComp({ color }) {
+  const [number, setNumber] = useState(0);
+  console.log(color + " : re render");
+
+  useEffect(() => {
+    console.log(color + " : initial render"); // 첫 렌더링 할때만 적용
+  }, []);
+
+  return (
+    <Box borderWidth={"10px"} borderColor={color}>
+      <Button onClick={() => setNumber(number + 1)}>증가</Button>
+      <Text>{number}</Text>
+    </Box>
+  );
+}
 
 function App(props) {
+  // 부모가 렌더링 될 때 자식도 렌더링 됨
   const [number, setNumber] = useState(0);
+  const [secondToggle, setSecondToggle] = useState(true);
+  const [gold, setGold] = useState("");
 
-  // useEffect 사용 시 자주 하는 실수
-  useEffect(() => {
-    console.log("코드 실행");
-    // setNumber(number + 1);
-    // trigger하는 값을 변경하면 안된다
-    // deps 값을 변경하는 코드를 짜면 안된다, 무한으로 리 렌더링 해버리기 때문에 큰일난다
-  }, [number]);
+  console.log("부모 re-render");
 
-  return <div></div>;
+  return (
+    <div>
+      <Box>
+        <Text>부모</Text>
+        <Button onClick={() => setNumber(number + 1)}>증가</Button>
+        <Text>{number}</Text>
+        <Checkbox
+          defaultChecked={true}
+          onChange={(e) => setSecondToggle(e.target.checked)}
+        ></Checkbox>
+        파란 박스 토글
+        <Checkbox
+          defaultChecked={true}
+          onChange={(e) => setGold(e.target.checked)}
+        ></Checkbox>
+        골드 박스 토글
+      </Box>
+      <Box mt={5}>
+        <Text>자식들</Text>
+        <MyComp color={"red"} />
+
+        {secondToggle && <MyComp color={"blue"} />}
+
+        <Box sx={{ display: gold ? "block" : "none" }}>
+          <MyComp color={"gold"} />
+        </Box>
+      </Box>
+    </div>
+  );
 }
 
 export default App;

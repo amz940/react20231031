7472e5009c8b17440602ead92d408f9e0f4f374a
dh_bug <dh_bug@naver.com>
2023-11-08 09:29:40 +0900

    useEffect의 두번째 파라미터는 첫번째 파라미터(setup, effect)를 실행시키는 트리거가 되는 값

--- src/App.js
+++ src/App.js
@@ -1,38 +1,24 @@
-import { useImmer } from "use-immer";
-import { Input, Text } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import { Button, Input, Text } from "@chakra-ui/react";
 
 function App(props) {
-  const [person, updatePerson] = useImmer({
-    name: "son",
-    address: { city: "seoul", country: "korea" },
-  });
+  const [number, setNumber] = useState(0);
+  const [text, setText] = useState("");
 
-  function handleNameChange(e) {
-    updatePerson((draft) => {
-      draft.name = e.target.value;
-    });
-  }
-
-  function handleCityChange(e) {
-    updatePerson((draft) => {
-      draft.address.city = e.target.value;
-    });
-  }
-
-  function handleCountryChange(e) {
-    updatePerson((draft) => {
-      draft.address.country = e.target.value;
-    });
-  }
+  // 첫번째 파라미터(set up) : The function with your Effect's logic.
+  // 부작용이 있는 함수(부작용이 나쁜뜻의 부작용이 아니라 부수적인 작용을 한다는 뜻)
+  // 두번재 파라미터를 안 쓰면 재 렌더링 될때마다 사용된다
+  // 두번째 파라미터 : set up(첫번째 파라미터)을 실행 시키는 값의 나열(배열)
+  // 빈 배열이면 첫 렌더링 때만 실행된다, 빈 배열이 아니라면 첫 렌더링 + 배열의 변화가 있을ㄸ마다 사용된다
+  useEffect(() => {
+    console.log("effect 함수 실행");
+  }, [number, text]);
 
   return (
     <div>
-      <Input value={person.name} onChange={handleNameChange} />
-      <Input value={person.address.city} onChange={handleCityChange} />
-      <Input value={person.address.country} onChange={handleCountryChange} />
-      <Text>
-        {person.name}은 {person.address.country}, {person.address.city}에 산다
-      </Text>
+      <Button onClick={() => setNumber(number + 1)}>증가</Button>
+      <Text>{number}</Text>
+      <Input value={text} onChange={(e) => setText(e.target.value)} />
     </div>
   );
 }

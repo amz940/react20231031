561ab4d9df641aaa98be96b5f0ea9b1191989217
dh_bug <dh_bug@naver.com>
2023-11-08 15:32:24 +0900

    링크 이동 시 react router의 Link를 사용 할 것

--- src/App.js
+++ src/App.js
@@ -2,16 +2,32 @@ import React from "react";
 import {
   createBrowserRouter,
   createRoutesFromElements,
+  Link,
   Outlet,
   Route,
   RouterProvider,
 } from "react-router-dom";
-import { Box } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 
 function HomeComponent() {
   return (
     <Box>
-      공통된 요소들
+      <Flex gap={"10px"}>
+        <Box>
+          {/*react-router 사용시 a 태그 사용 금지*/}
+          <a href="/apath">에이로 가기</a>
+        </Box>
+        <Box>
+          <a href="/bpath">비로 가기</a>
+        </Box>
+        {/*대신 Link Component 사용할 것*/}
+        <Box>
+          <Link to={"/apath"}>에이로 가기</Link>
+        </Box>
+        <Box>
+          <Link to={"/bpath"}>비로 가기</Link>
+        </Box>
+      </Flex>
       <Outlet />
     </Box>
   );
